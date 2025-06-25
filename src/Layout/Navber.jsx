e5e7199fe8ef8fe1396f3ec48e55bd860e459104
Md Jihadul Islam <zihadvai42@gmail.com>
2025-06-26 03:16:02 +0600

    add

--- src/Layout/Navber.jsx
+++ src/Layout/Navber.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { BsThreeDots } from "react-icons/bs";
 import { FaBars } from "react-icons/fa";
 import Cart from "../Icon/Cart";
+import SmsLogo from "../Icon/SmsLogo";
+import BelLogo from "../Icon/BelLogo";
+import Setting from "../Icon/Setting";
 
 const Navber = () => {
   return (
     <div>
-      <div className=" bg-[#615DFA] flex items-center px-[24px] py-[15px] justify-between">
-    
-
+      <div className=" bg-[#615DFA] flex items-center px-[24px] py-[15px] justify-between font-[Poppins]">
         <div className=" w-[90px] h-[40px] mr-[174px]">
           <a className="w-[90px] block" href="/" data-discover="true">
             <img src="images/logo.png" alt="logo" />
@@ -29,32 +30,44 @@ const Navber = () => {
           </ul>
         </div>
         <div className="">
-            <form action="#">
-              <input type="text"placeholder="Search here ...." className="w-[415px] h-[50px] pt-[15px] pb-[14px] px-4 bg-[#4E4AC8] rounded-[11px] outline-none focus:bg-white transition duration-700 ease-in-out . " />
-            </form>
+          <form action="#">
+            <input
+              type="text"
+              placeholder="Search here ...."
+              className="w-[415px] h-[50px] pt-[15px] pb-[14px] px-4 bg-[#4E4AC8] rounded-[11px] outline-none focus:bg-white transition duration-700 ease-in-out font-normal text-[#6965E0] text-[14px] "
+            />
+          </form>
         </div>
-        <div className=" flex items-center gap-[70px]  ">
-              <div className=" relative flex gap-[46px] after:absolute after:content-[''] after:w-full after:h-[3px] after:left-0 after:bottom-[-3px] after:bg-[#4E4AC8] after:rounded-[7px] after:z-0 before:absolute before:content-[''] before:w-[80%] before:h-[3px] before:left-0 before:bottom-[-3px] before:bg-[#41EFFF] before:rounded-[7px] before:z-10 ">
-                <span>Next</span>
-                <span>35 EXP</span>
-              </div>
-              <div className="flex gap-[33px] ">
-                <span>
-                  <Cart />
-                </span>
-                <span>
-                  <Cart />
-                </span>
-                <span>
-                  <Cart />
-                </span>
-                <div>
-                  <span>
-                    <Cart />
-                  </span>
-                </div>
-              </div>
+        <div
+          className="flex items-center gap-[70px] "
+        >
+          <div className="relative 
+  after:content-[''] after:absolute after:w-[1px] after:h-[32px] 
+  after:bg-[#7A77FD] after:top-[-5px] after:right-[-40px]">
+            <div className=" relative flex gap-[46px] after:absolute after:content-[''] after:w-full after:h-[3px] after:left-0 after:bottom-[-3px] after:bg-[#4E4AC8] after:rounded-[7px] after:z-0 before:absolute before:content-[''] before:w-[80%] before:h-[3px] before:left-0 before:bottom-[-3px] before:bg-[#41EFFF] before:rounded-[7px] before:z-10 font-normal text-white text-[12px] ">
+            <span>Next</span>
+            <span>35 EXP</span>
+          </div>
+          </div>
+          <div className="flex gap-[33px] ">
+            <span>
+              <Cart />
+            </span>
+            <span>
+              <SmsLogo />
+            </span>
+            <span>
+              <BelLogo />
+            </span>
+            <div className="ml-[33px] relative 
+  after:content-[''] after:absolute after:w-[1px] after:h-[32px] 
+  after:bg-[#7A77FD] after:top-[-5px] after:left-[-33px] ">
+              <span>
+                <Setting />
+              </span>
             </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/Layout/SideLeftBer.jsx
+++ src/Layout/SideLeftBer.jsx
@@ -2,8 +2,28 @@ import React from 'react'
 
 const SideLeftBer = () => {
   return (
-    <div className='w-[20%] h-40 bg-red-500'>
-      <button>hello</button>
+    <div className='w-[300px] h-full bg-[#ffffff] pt-[15px] pr-[8px] pl-[24px] font-[Poppins] '>
+      <div className='flex flex-col items-center mb-[42px]'>
+        <div className='w-[120px] h-[120px] rounded-full bg-[#615DFA] mb-[15px]'></div>
+        <h3 className='font-bold text-[16px] text-[#000000]'>Md Jihadul Islam</h3>
+        < p className='font-medium text-[12px] text-[#AFB0C0]'>www.jihad.com</p>
+      </div>
+      {/* title end  */}
+      <div className='flex gap-[49px] items-center text-center'>
+        <div>
+          <h4 className='font-bold text-[16px] text-[#000000]'>930</h4>
+          <span className='font-medium text-[12px] text-[#AFB0C0]'>Friends</span>
+        </div>
+        <div className='relative after:content-[""] after:w-[1px] after:h-[25px] after:absolute after:top-[50%] after:left-[-24px] after:bg-[#AFB0C0] after:-translate-y-1/2 before:content-[""] before:w-[1px] before:h-[25px] before:absolute before:top-[50%] before:right-[-24px] before:bg-[#AFB0C0] before:-translate-y-1/2'>
+          <h4 className='font-bold text-[16px] text-[#000000]'>58</h4>
+          <span className='font-medium text-[12px] text-[#AFB0C0]'>Post</span>
+        </div>
+        <div>
+          <h4 className='font-bold text-[16px] text-[#000000]'>1k</h4>
+          <span className='font-medium text-[12px] text-[#AFB0C0]'>Followers</span>
+        </div>
+      </div>
+      {/* ------end------- */}
     </div>
   )
 }
