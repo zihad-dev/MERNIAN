fa360f1cd101f68ba3a4db16dd8f7a8412a8523d
Md Jihadul Islam <zihadvai42@gmail.com>
2025-06-29 22:02:12 +0600

    add more

--- src/Component/ContentPost.jsx
+++ src/Component/ContentPost.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 import { BsThreeDots } from "react-icons/bs";
+import BelLogo from "../Icon/BelLogo";
+import LikeIcon from "../Icon/LikeIcon";
+import LoveIcon from "../Icon/LoveIcon";
+import Sad from "../Icon/Sad";
+import WowIcon from "../Icon/WowIcon";
+import { AiFillLike } from "react-icons/ai";
 
 const ContentPost = () => {
   return (
@@ -9,8 +15,12 @@ const ContentPost = () => {
           <div className="flex items-center gap-[15px]">
             <div className="w-[50px] h-[50px] rounded-full bg-[#615DFA] border-3 border-[#D9D9D9]"></div>
             <div>
-              <h4 className="font-[Poppins] font-bold text-[14px] text-[#303030]">Md Jihadul Islam</h4>
-              <p className="font-medium font-[Poppins] text-[12px] text-[#AFB0C0]">29 minutes ago</p>
+              <h4 className="font-[Poppins] font-bold text-[14px] text-[#303030]">
+                Md Jihadul Islam
+              </h4>
+              <p className="font-medium font-[Poppins] text-[12px] text-[#AFB0C0]">
+                29 minutes ago
+              </p>
             </div>
           </div>
           <BsThreeDots />
@@ -24,6 +34,38 @@ const ContentPost = () => {
             before the official release. This is a dream come true, thanks to
             all for the support!!!
           </p>
+          <div className="flex items-center justify-between">
+            <div className="flex items-center relative">
+              <div className="z-30">
+                <LikeIcon />
+              </div>
+              <div className="-ml-2 z-20">
+                <LoveIcon />
+              </div>
+              <div className="-ml-2 z-10">
+                <WowIcon />
+              </div>
+              <span className="font-[Inter] font-bold text-[12px] text-[#000000] ml-[13px] ">12</span>
+            </div>
+            <h5 className="font-['Inter'] font-bold text-[12px] text-[#000000] cursor-pointer">13 Comments</h5>
+            <span className="font-['Inter'] font-bold text-[12px] text-[#000000] cursor-pointer">2 Shares</span>
+          </div>
+        </div>
+      </div>
+      <div className="w-[100%] h-[1px] bg-white pt-[23px] pr-[41px] pb-[20px] pl-[20px] ">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center gap-4">
+            <AiFillLike />
+            <span>Like</span>
+          </div>
+          <div className="flex items-center gap-4">
+            <AiFillLike />
+            <span>Like</span>
+          </div>
+          <div className="flex items-center gap-4">
+            <AiFillLike />
+            <span>Like</span>
+          </div>
         </div>
       </div>
     </div>
