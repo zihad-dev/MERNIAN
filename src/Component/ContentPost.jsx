42d91f3be651452c25f7b3668a131f3250ec11b8
Md Jihadul Islam <zihadvai42@gmail.com>
2025-07-02 00:40:11 +0600

    add

--- src/Component/ContentPost.jsx
+++ src/Component/ContentPost.jsx
@@ -6,12 +6,13 @@ import LoveIcon from "../Icon/LoveIcon";
 import Sad from "../Icon/Sad";
 import WowIcon from "../Icon/WowIcon";
 import { AiFillLike } from "react-icons/ai";
+import { FaCommentAlt, FaShareAlt } from "react-icons/fa";
 
 const ContentPost = () => {
   return (
     <div>
-      <div className="bg-white pt-[15px] pr-[41px] pb-[20px] pl-[20px] rounded-[15px]">
-        <div className="flex items-center justify-between mb-[23px]">
+      <div className="bg-white pt-[15px] pr-[41px] pb-[20px] pl-[20px] rounded-t-[15px] ">
+        <div className="flex items-center justify-between mb-[23px] ">
           <div className="flex items-center gap-[15px]">
             <div className="w-[50px] h-[50px] rounded-full bg-[#615DFA] border-3 border-[#D9D9D9]"></div>
             <div>
@@ -23,7 +24,16 @@ const ContentPost = () => {
               </p>
             </div>
           </div>
-          <BsThreeDots />
+          <div className="relative">
+            <BsThreeDots className="text-[#000000] text-[20px] cursor-pointer" />
+            <div 
+            style={{ boxShadow: "0px 1px 4px 1px rgba(0, 0, 0, 0.11)" }}
+            className="w-[140px] h-[80px] bg-white absolute top-[20px] right-0 z-10 rounded-[7px]">
+              
+            </div>
+          </div>
+          
+          
         </div>
         <div>
           <p className="font-[Poppins] font-normal text-[12px] text-[#000000] max-w-[524px] relative after:absolute after:bottom-[-29px] after:left-0 after:content-[''] after:w-[100%] after:h-[1px] after:bg-[#D9D9D9] mb-[44px]">
@@ -52,19 +62,19 @@ const ContentPost = () => {
           </div>
         </div>
       </div>
-      <div className="w-[100%] h-[1px] bg-white pt-[23px] pr-[41px] pb-[20px] pl-[20px] ">
+      <div className="w-[100%] bg-[#FCFCFD] pt-[23px] pr-[41px] pb-[20px] pl-[20px] border-t border-[#D9D9D9]">
         <div className="flex items-center justify-between">
-          <div className="flex items-center gap-4">
-            <AiFillLike />
-            <span>Like</span>
+          <div className="flex items-center gap-4 cursor-pointer">
+            <AiFillLike className="text-[15px] text-[#D9D9D9]" />
+            <span className="font-[Inter] font-bold text-[#D9D9D9] text-[12px]">Like</span>
           </div>
-          <div className="flex items-center gap-4">
-            <AiFillLike />
-            <span>Like</span>
+          <div className="flex items-center gap-4 cursor-pointer">
+            <FaCommentAlt className="text-[15px] text-[#D9D9D9]" />
+            <span className="font-[Inter] font-bold text-[#D9D9D9] text-[12px]">Comment</span>
           </div>
-          <div className="flex items-center gap-4">
-            <AiFillLike />
-            <span>Like</span>
+          <div className="flex items-center gap-4 cursor-pointer">
+            <FaShareAlt className="text-[15px] text-[#D9D9D9]"/>
+            <span className="font-[Inter] font-bold text-[#D9D9D9] text-[12px]">Share</span>
           </div>
         </div>
       </div>
